e5431c1153852c05affab2714006db98783028d3
campper <alxor@live.cn>
2023-12-10 23:23:59 +0800

    add Binary Search Tree

--- data structure/BinarySearchTree/BinarySearchTree.cpp
+++ data structure/BinarySearchTree/BinarySearchTree.cpp
@@ -1,5 +1,59 @@
 //
 // Created by alxor on 10/12/2023.
 //
-
+#include <cstddef>
+#include "FileOps.h"
+#include "SequenceST.h"
 #include "BinarySearchTree.h"
+
+int main()
+{
+    string filename = "bible.txt";
+    vector<string> words;
+    if( FileOps::readFile(filename,words)){
+        cout<<"There are totally "<<words.size() <<" words in "<<filename;
+        cout <<endl;
+
+        //test BST
+        time_t startTime = clock();
+        BST<string,int> bst =  BST<string,int>();
+        for(vector<string>::iterator iter=words.begin();iter != words.end();iter++){
+            int *res = bst.search(*iter);
+            if(res == NULL){
+                bst.insert(*iter,1);
+            } else{
+                (*res)++;
+            }
+        }
+        cout<<"'god' :"<< *bst.search("god")<<endl;
+        time_t endTime = clock();
+        cout<<"BST time "<<double(endTime-startTime)/CLOCKS_PER_SEC<< " s." << endl;
+
+        // 测试顺序查找表 SST
+        startTime = clock();
+
+        // 统计圣经中所有词的词频
+        // 注: 这个词频统计法相对简陋, 没有考虑很多文本处理中的特殊问题
+        // 在这里只做性能测试用
+        SequenceST<string, int> sst = SequenceST<string, int>();
+        for (vector<string>::iterator iter = words.begin(); iter != words.end(); iter++) {
+            int *res = sst.search(*iter);
+            if (res == NULL)
+                sst.insert(*iter, 1);
+            else
+                (*res)++;
+        }
+
+        // 输出圣经中god一词出现的频率
+        if(sst.contain("god"))
+            cout << "'god' : " << *sst.search("god") << endl;
+        else
+            cout << "No word 'god' in " << filename << endl;
+
+        endTime = clock();
+
+        cout << "SST , time: " << double(endTime - startTime) / CLOCKS_PER_SEC << " s." << endl;
+
+    }
+    return 0;
+}
--- data structure/BinarySearchTree/BinarySearchTree.h
+++ data structure/BinarySearchTree/BinarySearchTree.h
@@ -17,7 +17,8 @@ private:
         Node(Key key,Value value){
             this->_key = key;
             this->_value = value;
-            this->_left = this->_right = NULL;
+            this->_left = NULL;
+            this->_right = NULL;
         }
 
     };
@@ -43,7 +44,65 @@ public:
     
     void insert(Key key,Value value){
 
-        insert();
+        _root = insert(_root,key,value);
+    }
+    bool contain(Key key){
+        return contain(this->_root,key);
+    }
+
+    //  Node* 
+    //  Value
+    //  指针可以为空元素
+    Value* search(Key key){
+        return search(this->_root,key);
+    }
+private:
+    // 向以node为根的二叉搜索树中,插入节点(key，value)
+    // 返回插入新节点后的二叉搜索树的根
+    Node* insert(Node *node,Key key,Value value){
+        if(node == NULL){
+            _count ++;
+            return new Node(key,value);
+        }
+
+        if(key == node->_key){
+            node->_value = value;
+        } else if(key < node->_key){
+            node->_left = insert(node->_left,key,value);
+        } else {
+            node->_right = insert(node->_right,key,value);
+        }
+
+        return node;
+
+    }
+
+    // 查看以node为根的二叉搜索树种是否包含键值为key的节点
+    bool contain(Node* node,Key key){
+        if(node == NULL){
+            return false;
+        }
+
+        if(key == node->_key){
+            return true;
+        } else if (key < node->_key){
+            return contain(node->_left,key);
+        } else {
+            return contain(node->_right,key);
+        }
+    }
+
+    Value* search(Node* node,Key key){
+        if(node == NULL){
+            return NULL;
+        }
+        if(key == node->_key){
+            return &(node->_value);
+        } else if (key < node->_key){
+            return search(node->_left,key);
+        } else {
+            return search(node->_right,key);
+        }
     }
 
 };
