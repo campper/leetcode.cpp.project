cc2c65416984c9879b3a8d94e8a9c49575af71ab
alxor <campxp@hotmail.com>
2025-02-23 15:43:58 +0800

    update algorithm leetcode

---
+++ algorithms/20.有效的括号.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=cpp
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+            st.push(c);
+            } else {
+            if (st.empty()) {
+                return false;
+            }
+            if (c == ')' && st.top() != '(') {
+                return false;
+            }
+            if (c == ']' && st.top() != '[') {
+                return false;
+            }
+            if (c == '}' && st.top() != '{') {
+                return false;
+            }
+            st.pop();
+            }
+        }
+        return st.empty();
+        
+    }
+};
+// @lc code=end
+
