56d0aaedf8b4b48bf0db8a20aca6c3da99c3cd77
alxor <alxor@live.cn>
2025-03-06 22:42:49 +0800

    修改主题目录

---
+++ themes/sgcc/sgcc-theme/layouts/index.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { handleBackground, resolveAssetUrl } from "./layoutHelper";
+
+const getCurrentDate = (): String => {
+  const now = new Date();
+  return now.getFullYear() + "年" + (now.getMonth() + 1) + "月";
+};
+
+
+const props = defineProps({
+  background: {
+    type: String,
+    default: "/assets/sgcc_title_bg.png"
+  },
+  headtitle: {
+    type: String,
+    default: "光明电力大模型",
+  },
+  subtitle: {
+    type: String,
+    default: "中国电力科学研究院有限公司",
+  },
+  publishTime: {
+    type: String,
+    default: Date().toString(),
+  },
+  author: {
+    type: String,
+    default: "张强",
+  },
+
+});
+
+const imageSrc = "/assets/SGGM-logo.png";
+const imgWidth = 66;
+const imgHeight = 66;
+
+const style = handleBackground(
+    props.background,
+    true,
+    // "linear-gradient(90deg,rgba(3,10,22,1) 0%, rgba(59,130,246,0.5) 100%)"
+);
+</script>
+
+<template>
+    <div class="slidev-layout cover" :style="style">
+        <div class="-ml-4 flex flex-row items-center space-x-3 h-[2.8em]">
+            <div><img src="/assets/sgcc_logo.png" class="h-[5em]" /></div>
+        </div>
+      
+        <div class="ml-30 my-auto w-full mb-17 pl-9 border-l-3" >
+          <div class="flex items-left space-x-5 h-[5em]">
+            <img :src="imageSrc" :style="{ width: imgWidth + 'px', height: imgHeight + 'px' }" alt="光明大模型" />
+          </div>
+          <h1 class="tracking-wide font-normal" style="font-size: 3rem;">{{ props.headtitle }}</h1>
+          <span class="tracking-wide text-2xl font-normal" style="color:#01796a; font-size: 2rem;">{{ props.subtitle }}</span>
+        </div>
+        <div class="mt-2 mb-4 pb-0 -mb-20">
+        <p class="tracking-wide font-serif text-2xl font-semibold" style="position:fixed; bottom: 100px; left: 100px;">{{ getCurrentDate() }}</p>
+        </div>
+    </div>
+</template>
