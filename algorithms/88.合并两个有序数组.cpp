3e99fec1038a555215c5334089371ded52df0c3e
alxor <alxor@live.cn>
2025-03-24 20:04:11 +0800

    优化SelectionSort的验证测试函数

--- algorithms/88.合并两个有序数组.cpp
+++ algorithms/88.合并两个有序数组.cpp
@@ -9,14 +9,12 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         // 方法一 ： 合并后排序
-        for (int i = 0; i < n; i ++) {
-            nums1[m + i] = nums2[i];
+        for(int i = 0; i < n; i++){
+            nums1[m+i] = nums2[i];
         }
-        
         sort(nums1.begin(), nums1.end());
-
-        
     }
+
 
     
 };
--- java/03-Selection-Sort/src/Main.java
+++ java/03-Selection-Sort/src/Main.java
@@ -1,22 +1,31 @@
+import javax.swing.*;
+
 public class Main {
+
     public static <E extends Comparable<E>> void main(String[] args) {
-        Integer[] arr = {1,4,2,3,5};
-        SelectionSort.sort(arr);
-        for(int e:arr){
-            System.out.print(e + " ");
+//        Integer[] arr = {1,4,2,3,5};
+//        SelectionSort.sort(arr);
+//        for(int e:arr){
+//            System.out.print(e + " ");
+//        }
+//        System.out.println();
+//
+//        Student[] students = new Student{
+//            new Student("Alice",98),
+//            new Student("Bobo",100),
+//            new Student("Charles",66)};
+//
+//        SelectionSort.sort(students);
+//        for(Student s:students){
+//            System.out.print(s + " ");
+//        }
+//        System.out.println();
+
+        int[] dataSize = {10000,100000};
+        for(int n:dataSize)
+        {
+            Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
+            SortingHelper.sortTest("SelectionSort", arr);
         }
-        System.out.println();
-
-        Student[] students = new Student{
-            new Student("Alice",98),
-            new Student("Bobo",100),
-            new Student("Charles",66)};
-
-        SelectionSort.sort(students);
-        for(Student s:students){
-            System.out.print(s + " ");
-        }
-        System.out.println();
-
     }
 }
