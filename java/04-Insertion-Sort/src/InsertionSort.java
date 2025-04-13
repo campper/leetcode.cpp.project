ea18615652ba00e488695b1b813b2627f6875ee8
alxor <alxor@live.cn>
2025-04-13 11:24:25 +0800

    新增InsertionSort 插入排序算法

--- java/04-Insertion-Sort/src/InsertionSort.java
+++ java/04-Insertion-Sort/src/InsertionSort.java
@@ -5,14 +5,15 @@ public class InsertionSort {
         for(int i=0;i<arr.length;i++){
 
             // 将arr[i] 插入到合适的位置
-
-            for(int j=i;j - 1 >= 0;j--){
-
-                if(arr[j].compareTo(arr[j-1])<0)
-                    swap(arr,j,j-1);
-                else break;
-
-            }
+//            for(int j=i;j - 1 >= 0;j--){
+//
+//                if(arr[j].compareTo(arr[j-1])<0)
+//                    swap(arr,j,j-1);
+//                else break;
+//
+//            }
+            for(int j=i;j - 1 >= 0 && arr[j].compareTo(arr[j-1]) < 0; j--)
+                swap(arr,j,j-1);
             // System.out.println(arr[i]);
         }
     }
